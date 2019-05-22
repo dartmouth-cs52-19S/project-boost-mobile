fe0f2438bb582a9042b0828901be8f0e3fbeaa1a
Varsha Iyer <varsha.b.iyer@gmail.com>
2019-05-22 00:44:53 -0400

    finished settings and initial setup

--- screens/ProvideInitialInfoScreen.js
+++ screens/ProvideInitialInfoScreen.js
@@ -171,6 +171,7 @@ export default class ProvideInitialInfo extends React.Component {
             styles={{
               description: {
                 fontWeight: 'bold',
+                color: '#293C44',
               },
               textInputContainer: {
                 width: '100%',
@@ -237,7 +238,7 @@ export default class ProvideInitialInfo extends React.Component {
                           style={styles.switch}
                           value={this.state[key]}
                           onValueChange={value => this.toggleSwitch(i, value)}
-                          trackColor={{ true: '#293C44' }}
+                          trackColor={{ true: '#388CAB' }}
                           ios_backgroundColor="#388CAB"
                         />
                         <Text style={styles.switchText}>YES</Text>
@@ -317,6 +318,7 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   columnText: {
+    paddingTop: 5,
     color: '#FEFEFE',
     fontSize: 20,
     fontFamily: 'Raleway-Light',
@@ -329,10 +331,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginLeft: 10,
     marginRight: 10,
+    borderWidth: 0.5,
+    borderColor: '#FEFEFE',
+    color: 'rgba(0,0,0,0)',
   },
   switchText: {
-    color: '#FEFEFE',
     fontSize: 18,
+    color: '#E5E5E5',
   },
   switchContainer: {
     flexDirection: 'row',
